10443310456561a11e1df06d5eb6bf406142aed4
Connor Ludwigson <connorlud@CL-MBP15.local>
2020-10-19 16:50:15 -0500

    set up login/logout

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private flashMessageService: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if ((<any>data).success) {
+        this.authService.storeUserData((<any>data).token, (<any>data).user);
+        this.flashMessageService.show("You have successfully logged in!", {cssClass: 'alert-success'});
+        this.router.navigate(['/profile']);
+      } else {
+        this.flashMessageService.show("Something didn't work", {cssClass: 'alert-danger'});
+        this.router.navigate(['/login']);
+      }
+    });
+    
   }
 
 }
