9bf403af0c7852b0f0e3e530ade6498aad4f443a
Connor Ludwigson <ludwigso.connor@uwlax.edu>
2020-10-20 21:39:54 -0500

    time formatting on programs view

--- angular-src/src/app/components/programs/programs.component.ts
+++ angular-src/src/app/components/programs/programs.component.ts
@@ -27,10 +27,27 @@ export class ProgramsComponent implements OnInit {
     });
     this.programService.loadPrograms().subscribe(programs => {
       this.programs = (<any>programs);
+      (<any>programs).forEach(program => {
+        program.startTime = this.convertMilitaryto12Hr(program.startTime);
+        program.endTime = this.convertMilitaryto12Hr(program.endTime);
+      });
     },
     error => {
       console.log(error);
       return false;
     });
+  }
+  
+  private convertMilitaryto12Hr(time: string) {
+    let hr: number= parseInt(time);
+    let min: string = time.substring(2);
+    let half: string;
+    if (hr > 12) {
+      hr -= 12;
+      half = " PM";
+    } else {
+      half = " AM";
+    }
+    return hr + min + half;
   }
 }
